be1774e5f1b8128b543707a05e8f0e790572a1c8
YS7Creates <ysyashmks@gmail.com>
2024-03-15 15:10:08 +0530

    js

--- JS/Day5_Array.js
+++ JS/Day5_Array.js
@@ -1,13 +1,21 @@
 // array
 const Arr = [  2,3,0 ]
 //or use it as an object
-const arr2 = new Array ( 2,3,6 )
-console.log(arr2[2]);
+const arr2 = new Array ( 2,3,6,21,4,1,6,7,2,9 )
+// console.log(arr2[2]);
 
 
 // Methods
 
-// arr2.push(5); // inserts value in Array
-// arr2.pop(); // removes the last value of the array
-arr2.shift(3)
-console.log(arr2);
+ arr2.push(5); // inserts value in Array
+ arr2.pop(); // removes the last value of the array
+ arr2.unshift(5) // aage daldeta hai ek value
+ arr2.shift(); // removes the first value 
+
+ console.log(arr2.slice(1)); // value kat deta h 
+ console.log(arr2.includes(6));
+const ar2 = arr2.join()
+ console.log(arr2.splice(0,3)); // basically prints the value btw the given index
+ console.log(arr2.slice(0,4)); // removes all the values btw the give range exlcuding the last term and print everything
+ console.log(ar2);
+ console.log(typeof(ar2));
