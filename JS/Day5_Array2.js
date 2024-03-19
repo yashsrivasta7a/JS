490236e523722adbf14ea25d77ae0131bc5dfa66
YS7Creates <ysyashmks@gmail.com>
2024-03-20 00:22:20 +0530

    new changes

--- JS/Day5_Array2.js
+++ JS/Day5_Array2.js
@@ -5,17 +5,20 @@ const c = [ " arnav " , "Sankit ", " Manikanta"]
 // Array.of =  makes array of the data type
 // Array.from = makes array like yash ka [y,a,s,h]
 
-const fried =friends.concat(c) 
-console.log(fried);
-
-const friend = [...friends, ...c]
-console.log(friend);
-
-console.log(Array.from("yash")) //basically creates the string or other data type into the array.
-console.log(Array.from({name : " yash "})) //here kisko array banana is not defined so phle usse define krenge then work krenge
-
 let score2 = 100
 let score1 = 200
 let score5 = 500
 
+const fried =friends.concat(c) 
+console.log(fried);
+
+const array1 = [1, 2, 3];
+const array2 = [4, 5, 6];
+const newArray = array1.concat(array2);
+
+const friend = [...friends, ...c] // ... select karleta hai saare elements ko 
+
+console.log(friend);
+console.log(Array.from("yash")) //basically creates the string or other data type into the array.
+console.log(Array.from({name : " yash "})) //here kisko array banana is not defined so phle usse define krenge then work krenge
 console.log(Array.of(score2,score1,score5)); // makes the array from the values
--- JS/objects.js
+++ JS/objects.js
@@ -1,6 +1,7 @@
 // singleton - koi bhi constructor se banate haii , dusri tareeke se banane pr multiple instances banega
 
 // object literals
+// square brackets lagte h for calling and defining Symbol
 
 const Symvol = Symbol("Key1")
 const user = {
@@ -10,12 +11,35 @@ age : 19 ,
 location : "gurgaon" ,
 isLoggedIn: true,
 LastLoginDay: ["Monday","Saturday"]
-
 };
-console.log(user["isLoggedIn"]);
-console.log(user[Symvol]);
-Object.freeze(user) // freeze the content so koi changes nhi hoenge
 
-user.greeting = function (){
+// console.log(user["isLoggedIn"]);
+// console.log(user[Symvol]); 
+
+// Object.freeze(user) // freeze the content so koi changes nhi hoenge
+
+user.Greeting = function(){
     console.log(" Hello User ");
+} 
+user.Greeting2 = function(){
+    console.log(`Hello User, ${this.name}`);
 }
+user.Greeting() , user.Greeting2()
+
+const ob1 = {
+    Y :"yash",
+    S :"srivastava"
+}
+const ob2 = {
+    W:"whats",
+    U:"Yours"
+} 
+// const ob3 = Object.assign(ob1 , ob2) // combines object's values
+// console.log(ob3);
+// // or we can use 
+// const ob3 = {...ob1 , ...ob2}
+
+console.log(Object.values(ob1));
+console.log(Object.entries(ob1));
+console.log(Object.keys(ob1));
+console.log(ob1.hasOwnProperty('Y')); // to check if some property or key is there or not 
