f4a715ced54f69aa6a106b3277ea18f1925b6a82
YS7Creates <ysyashmks@gmail.com>
2024-03-24 10:17:54 +0530

    Addon

--- JS/scopes.js
+++ JS/scopes.js
@@ -7,15 +7,40 @@ if(true){
 }
 console.log("Global Scope : " , a); // this is a global scope 
 
-console.log(addone(5))
 
-function addone(num){
-    return num + 1
+function one(){
+    const username = "Yash" 
+
+    function two() 
+    {
+    const website = " YT"
+    console.log(username);
+    }
+    // console.log(website);
+    two()
+}
+one()
+
+
+if(true){
+    const username = "yash"
+    if(username === "yash"){
+        const website = " youtube "
+        console.log(username + website);
+    }
+    // console.log(website);
+}
+// console.log(username);
+
+
+function addon(value)
+{
+return ++value;
 }
 
+console.log(addon(5));
 
-
-addTwo(5)
-const addTwo = function(num){
-    return num + 2
+const addtwo = function(num){
+    return num+2 ;
 }
+console.log( addtwo(5) );
