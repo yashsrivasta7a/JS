11fd73337b8cfc418195542268989cb0b03e528c
YS7Creates <ysyashmks@gmail.com>
2024-03-27 21:12:48 +0530

    n

---
+++ JS/for.js
@@ -0,0 +1,23 @@
+// for of
+const ar=[5,6,0];
+const ob = {
+    username:"yash"
+    ,
+    rollno:5
+}
+let obj = Object.keys(ob)
+// for loop without condition and increment
+for (const val of obj) {
+    console.log(val);    
+}
+// 
+const greeting = "hello Hello"
+for (const greet of greeting) {
+    if(greet == " "){
+        continue;
+    }
+    else {
+        console.log(`${greet}`);
+    }
+}
+
