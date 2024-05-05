71dec928a2145502cc14837e4d6481abae29ba89
Yash Srivastava <120123563+yashsrivasta7a@users.noreply.github.com>
2024-05-05 12:11:59 +0530

    backend

--- Express/Backend/index.js
+++ Express/Backend/index.js
@@ -1,29 +1,32 @@
-require('dotenv').config()
-const { log } = require('console')
-const express = require('express')
-const app = express()
+require("dotenv").config();
+const { log } = require("console");
+const express = require("express");
+const app = express();
+const cors = require('cors')
 
-app.get('/api/jokes',(req ,res) => {
-    const jokes = [{
-        "id":1,
-        "title":'Joke1',
-        "content":'Joke1hahaha'
+
+app.get("/api/jokes", (req, res) => {
+  const jokes = [
+    {
+      id: 1,
+      title: "Joke1",
+      content: "Joke1hahaha",
     },
     {
-        "no":2,
-        "title":'Joke2',
-        "content":'Joke2hahaha'
+      no: 2,
+      title: "Joke2",
+      content: "Joke2hahaha",
     },
     {
-        "no":3,
-        "title":'Joke3',
-        "content":'Joke3hahaha'
+      no: 3,
+      title: "Joke3",
+      content: "Joke3hahaha",
     },
-]
-    res.send (jokes);
-})
-const port = process.env.PORT || 3000
+  ];
+  res.send(jokes);
+});
+const port = process.env.PORT || 3000;
 
-app.listen( port , ()=>{
-    console.log(`${port}`);
-})
+app.listen(port, () => {
+  console.log(`${port}`);
+});
